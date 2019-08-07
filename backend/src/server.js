87e288a627e47abd7b4a9f9ca8c627033ee7fe82
allyson <vhassan@outlook.com>
2019-08-07 11:59:22 -0300

    Backend

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0-3d4nh.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
